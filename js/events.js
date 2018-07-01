b3715d066e4908e64aac01f0ff588f00e657a9c3
Rachel Dodell <rachel@racheldodell.com>
2018-07-01 01:47:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,15 +1,29 @@
 //define functions here
-
-ar getIt=$('p').on('click', function () { 
-+  alert("Hey!") 
-+  return; 
-+}) 
+function getIt() {
++  $('p').on("click", function () {
++    alert("Hey!");
++  }
++)}
 +
-+var frameIt=$('img').on('load', 
-+function() {
-+  add('.tasty'); 
-+  img.style.bordercolor='red';
-+})
++function frameIt () {
++  $('img').on("load", function () {
++    $('img').addClass("tasty");
++  });
++}
++
++function pressIt () {
++ $(document).on('keydown', function(key) {
++  if(key.which === 71){
++      alert('G was pressed');
++  }
++});
++}
++
++function submitIt() {
++  $('form').on("submit", function() {
++    alert("Your form is going to be submitted now.");
++});
++}
 
 $(document).ready(function(){
 
