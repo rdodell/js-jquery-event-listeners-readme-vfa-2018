a5da7dfd3cb597ca55fd6b4bd8042f249d00f21b
Rachel Dodell <rachel@racheldodell.com>
2018-07-01 01:47:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,16 @@
 //define functions here
 
+ar getIt=$('p').on('click', function () { 
++  alert("Hey!") 
++  return; 
++}) 
++
++var frameIt=$('img').on('load', 
++function() {
++  add('.tasty'); 
++  img.style.bordercolor='red';
++})
+
 $(document).ready(function(){
 
 // call functions here
