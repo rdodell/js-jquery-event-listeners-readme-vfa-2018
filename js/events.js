60aa6936dcda0ed6df4ad6ee51196064f2ed34d5
Rachel Dodell <rachel@racheldodell.com>
2018-07-01 01:48:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,21 +8,21 @@ function getIt() {
 
 function frameIt () {
   $('img').on("load", function () {
-+    $('img').addClass("tasty");
-+  });
-+}
-+
-+function pressIt () {
-+ $(document).on('keydown', function(key) {
-+  if(key.which === 71){
-+      alert('G was pressed');
-+  }
-+});
-+}
-+
-+function submitIt() {
-+  $('form').on("submit", function() {
-+    alert("Your form is going to be submitted now.");
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt () {
+ $(document).on('keydown', function(key) {
+  if(key.which === 71){
+      alert('G was pressed');
+  }
+});
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
 +});
 +}
 
